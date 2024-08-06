ca1af7281d8402a48fc849b972da2401d0068b60
muhammadsyafiq1 <syafiqmuhammad655@gmail.com>
2024-08-06 14:58:13 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from "express"
+import fileUpload from "express-fileupload"
+import cors from "cors"
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use(fileUpload())
+
+ app.listen(5000, () => {
+    console.log('Server berhasil dijalankan');
+ });
